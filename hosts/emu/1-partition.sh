16feec3d91f7a3666843332eda3f642d6352d73a
0x42697262 <19298806+0x42697262@users.noreply.github.com>
2025-07-14 21:46:49 +0900

    add script for partitioning with luks

--- hosts/emu/1-partition.sh
+++ hosts/emu/1-partition.sh
@@ -15,8 +15,42 @@ else
     exit 1
 fi
 
+echo "CachyOS Device: $CACHYOS_DEVICE"
 
+sudo parted $CACHYOS_DEVICE -- mklabel gpt
+sudo parted $CACHYOS_DEVICE -- mkpart ESP fat32 1MiB 513MiB
+sudo parted $CACHYOS_DEVICE -- set 1 esp on
+sudo parted $CACHYOS_DEVICE -- mkpart CachyOS ext4 513MiB 100%
 
+sleep 2
+BOOT_PARTITION=$(lsblk -no PATH,PARTLABEL "$CACHYOS_DEVICE" | grep 'ESP' | awk '{print $1}')
+LUKS_CONTAINER=$(lsblk -no PATH,PARTLABEL "$CACHYOS_DEVICE" | grep 'CachyOS' | awk '{print $1}')
 
+sudo mkfs.fat -F 32 $BOOT_PARTITION
+echo "[!] Formatted (${BOOT_PARTITION}) as boot partition."
 
+echo "[+] To start encrypting ${LUKS_CONTAINER} partition with LUKS2."
+
+if [[ -z "$LUKS_CONTAINER" ]]; then
+    echo "[X] Could not find CachyOS partition. Exiting."
+    exit 1
+fi
+
+sudo cryptsetup --verify-passphrase -v luksFormat $LUKS_CONTAINER
+sudo cryptsetup open $LUKS_CONTAINER cryptroot
+
+CRYPTROOT=/dev/mapper/cryptroot
+
+echo "[+] Creating Physical Volume on ${CRYPTROOT}"
+sudo pvcreate $CRYPTROOT
+echo "[+] Creating Volume Group on ${CRYPTROOT}"
+sudo vgcreate vg0 $CRYPTROOT
+echo "[+] Creating logical volumes"
+sudo lvcreate -L 128G -n root vg0
+sudo lvcreate -L 512G -n home vg0
+sudo lvcreate -l 100%FREE -n isekai vg0
+
+sudo mkfs.ext4 /dev/vg0/root
+sudo mkfs.ext4 /dev/vg0/home
+sudo mkfs.ext4 /dev/vg0/isekai
 
